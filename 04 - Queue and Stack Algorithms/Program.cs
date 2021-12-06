4c7e173e952e91f2b2f05762bca314b81682a2cb
Kurohige <jhidalgopalacios@yahoo.com>
2021-12-06 19:24:40 +0200

    commit on master

---
+++ 04 - Queue and Stack Algorithms/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+    ~~~~ 04 - Queue and Stack Algorithms ~~~~~~~
+    Completed by Jose O Hidalgo on 12/06/2021
+*/
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+
+    }
+}
